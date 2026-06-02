dd184ca901174eb74617630134f7f750155bd24b
mattiabusiness <mattia.businessgrowth@gmail.com>
2026-06-02 12:35:02 +0200

    fix: enlarge marketing logo in navbar (52px) and footer (48px)

--- apps/aegis-beauty/app/(marketing)/_components/Navbar.tsx
+++ apps/aegis-beauty/app/(marketing)/_components/Navbar.tsx
@@ -62,7 +62,7 @@ export function Navbar() {
           {/* Logo */}
           <Link href="/" style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/logo-marketing.png" alt="Aegis Beauty" style={{ height: 40, width: 'auto', display: 'block' }} />
+            <img src="/logo-marketing.png" alt="Aegis Beauty" style={{ height: 52, width: 'auto', display: 'block' }} />
           </Link>
 
           {/* Desktop nav */}
